67be29f72d52c0c3196fd1f5177ad2dabd38ad51
Endrit-Selita <eselita2001@gmail.com>
2025-09-25 23:45:47 +0100

    fixed ecs ingress and egress

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-# S3 Bucket
+############### S3 Bucket ###############
 terraform {
   backend "s3" {
     bucket = "mys3bucket"
@@ -23,8 +23,8 @@ terraform {
   }
 }
 
-# Create Route 53
-################ need to fix godaddy to make this work
+############### Create Route 53 ###############
+# need to fix godaddy to make this work
 resource "aws_route53_zone" "r53_zone" {
   name = "normanbrown.co.uk"
 }
@@ -41,7 +41,14 @@ resource "aws_route53_record" "www" {
   }
 }
 
-# Create a VPC
+############### ACM ###############
+
+resource "aws_acm_certificate" "acm_cert" {
+  domain_name       = "tm.normanbrown.co.uk"
+  validation_method = "DNS"
+}
+
+############### Create a VPC ###############
 resource "aws_vpc" "vpc-ecs" {
   cidr_block = "10.10.0.0/16"
 }
@@ -98,7 +105,78 @@ resource "aws_nat_gateway" "nat-gw2" {
   subnet_id     = aws_subnet.public2.id
 }
 
-# ALB
+# private subnet 1 in AZ1
+resource "aws_subnet" "private1" {
+  cidr_block              = "10.13.0.0/16"
+  vpc_id                  = aws_vpc.vpc-ecs.id
+  map_public_ip_on_launch = false
+  availability_zone = data.aws_availability_zones.az.names[0]
+}
+
+# private subnet 2 in AZ2
+resource "aws_subnet" "private2" {
+  cidr_block              = "10.14.0.0/16"
+  vpc_id                  = aws_vpc.vpc-ecs.id
+  map_public_ip_on_launch = false
+  availability_zone = data.aws_availability_zones.az.names[1]
+}
+
+# route table internet gateway
+resource "aws_route_table" "ig_rt_public" {
+  vpc_id = aws_vpc.vpc-ecs.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.i-gateway.id
+  }
+}
+
+#public subnet 1 association to internet gateway route table
+resource "aws_route_table_association" "pubsub1-igr" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.ig_rt_public.id
+}
+
+#public subnet 2 association to internet gateway route table
+resource "aws_route_table_association" "pubsub2-igr" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.ig_rt_public.id
+}
+
+# route table NAT Gateway in public subnet 1
+resource "aws_route_table" "ng_rt_ecs_1" {
+  vpc_id = aws_vpc.vpc-ecs.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw1.id
+  }
+}
+
+# private subnet 1 association to NAT Gateway route table 1
+resource "aws_route_table_association" "prisub1-ngr" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.ng_rt_ecs_1.id
+}
+
+# route table NAT Gateway in public subnet 2
+resource "aws_route_table" "ng_rt_ecs_2" {
+  vpc_id = aws_vpc.vpc-ecs.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw2.id
+  }
+}
+
+# private subnet 2 association to NAT Gateway route table 2
+resource "aws_route_table_association" "prisub2-ngr" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.ng_rt_ecs_2.id
+}
+
+
+############### ALB ###############
 resource "aws_lb" "alb-ecs" {
   name               = "alb-ecs"
   internal           = false
@@ -193,28 +271,7 @@ resource "aws_vpc_security_group_egress_rule" "alb_egress" {
   ip_protocol       = "-1" 
 }
 
-resource "aws_acm_certificate" "acm_cert" {
-  domain_name       = "tm.normanbrown.co.uk"
-  validation_method = "DNS"
-}
-
-# private subnet 1 in AZ1
-resource "aws_subnet" "private1" {
-  cidr_block              = "10.13.0.0/16"
-  vpc_id                  = aws_vpc.vpc-ecs.id
-  map_public_ip_on_launch = false
-  availability_zone = data.aws_availability_zones.az.names[0]
-}
-
-# private subnet 2 in AZ2
-resource "aws_subnet" "private2" {
-  cidr_block              = "10.14.0.0/16"
-  vpc_id                  = aws_vpc.vpc-ecs.id
-  map_public_ip_on_launch = false
-  availability_zone = data.aws_availability_zones.az.names[1]
-}
-
-############# Creating ECS ##########################
+############### Creating ECS ###############
 
 # ECS Cluster
 resource "aws_ecs_cluster" "ecs_cluster" {
@@ -276,8 +333,8 @@ resource "aws_ecs_task_definition" "service" {
 
   container_definitions = jsonencode([
     {
-      name      = "first"
-      image     = "service-first"
+      name      = "tcdf"
+      image     = "${aws_ecr_repository.ecr_app.repository_url}:latest"
       cpu       = 10
       memory    = 512
       essential = true
@@ -287,8 +344,16 @@ resource "aws_ecs_task_definition" "service" {
           hostPort      = 80
         }
       ]
-    }, ])
-
+    logConfiguration = {
+      logDriver                 = "awslogs",
+      options: {
+        awslogs-group           = aws_cloudwatch_log_group.ecs_cloudwatch.name,
+        awslogs-region          = "eu-west-2",
+        awslogs-stream-prefix   = "ecs"
+      }
+      }
+    }
+  ])
 }
 
 resource "aws_ecs_service" "ecs_service" {
@@ -296,17 +361,17 @@ resource "aws_ecs_service" "ecs_service" {
   launch_type     = "FARGATE"
   platform_version = "LATEST"
   cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.service.id
+  task_definition = aws_ecs_task_definition.service.arn
   desired_count   = 2
 
   load_balancer {
     target_group_arn = aws_lb_target_group.alb_targetgroup.id
-    container_name   = "latest-container"
-    container_port   = 443
+    container_name   = "tcdf"
+    container_port   = 80
   }
 
   network_configuration {
-    assign_public_ip = true
+    assign_public_ip = false
     security_groups = [aws_security_group.ecs_sg.id]
     subnets          = [aws_subnet.private1.id, aws_subnet.private2.id]
   }
@@ -316,38 +381,28 @@ resource "aws_security_group" "ecs_sg" {
   name        = "ecs_sg"
   description = "Allow HTTP inbound traffic from ALB"
   vpc_id      = aws_vpc.vpc-ecs.id
+
+ingress {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      security_groups = [aws_security_group.alb_sg.id]
+      }
+
+egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ecs_http" {
-  security_group_id = aws_security_group.ecs_sg.id
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
-}
-
-resource "aws_vpc_security_group_egress_rule" "ecs_egress" {
-  security_group_id = aws_security_group.ecs_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
-}
-
-# ECR 
+############### ECR ###############
 resource "aws_ecr_repository" "ecr_app" {
   name                 = "ecr_app"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
-  }
-}
-
-# route table internet gateway
-resource "aws_route_table" "rt_public" {
-  vpc_id = aws_vpc.vpc-ecs.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.i-gateway.id
   }
 }
