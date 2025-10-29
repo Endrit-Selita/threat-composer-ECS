452cf4c140ad6a7332a7885e4e9c5195186c0089
Endrit-Selita <eselita2001@gmail.com>
2025-10-29 20:26:37 +0000

    added variables to ALB module

--- terraform/modules/ALB/main.tf
+++ terraform/modules/ALB/main.tf
@@ -1,33 +1,33 @@
 ############### ALB ###############
 resource "aws_lb" "alb-ecs" {
-  name               = "alb-ecs"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  name               = var.alb-ecs-name
+  internal           = var.alb-ecs-internal
+  load_balancer_type = var.alb-ecs-load_balancer_type
+  security_groups    = [aws_security_group.alb_sg.id] 
   subnets            = [ 
     aws_subnet.public1.id,
     aws_subnet.public2.id]
 
-  enable_deletion_protection = false
+  enable_deletion_protection = var.alb-ecs-enable_deletion_protection
 }
 
 resource "aws_lb_target_group" "albtargetgroup" {
-  name        = "albtargetgroup"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
+  name        = var.aws_lb_target_group_name
+  port        = var.aws_lb_target_group_port
+  protocol    = var.aws_lb_target_group_protocol
+  target_type = var.aws_lb_target_group_target_type
   vpc_id      = aws_vpc.vpc-ecs.id
 
 health_check {
-  enabled = true
-  interval = 30
-  path = "/"
-  port = "traffic-port"
-  protocol = "HTTP"
-  timeout = 5
-  healthy_threshold = 3
-  unhealthy_threshold = 3
-  matcher = "200-299"
+  enabled = var.health_check_enabled
+  interval = var.health_check_interval
+  path = var.health_check_path
+  port = var.health_check_port
+  protocol = var.health_check_protocol
+  timeout = var.health_check_timeout
+  healthy_threshold = var.health_check_healthy_threshold
+  unhealthy_threshold = var.health_check_unhealthy_threshold
+  matcher = var.health_check_matcher
 }
 }
 
@@ -35,7 +35,7 @@ resource "aws_lb_listener" "HTTPS_l" {
   load_balancer_arn = aws_lb.alb-ecs.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = var.HTTPS_l_ssl_policy
   certificate_arn   = aws_acm_certificate.acm_cert.arn
   
   default_action {
@@ -61,7 +61,7 @@ resource "aws_lb_listener" "HTTP_1" {
 }
 
 resource "aws_security_group" "alb_sg" {
-  name        = "alb_sg"
+  name        = var.aws_security_group_name
   description = "ALB Security Group"
   vpc_id      = aws_vpc.vpc-ecs.id
   tags = {
@@ -72,24 +72,24 @@ resource "aws_security_group" "alb_sg" {
 resource "aws_vpc_security_group_ingress_rule" "alb_ingress_http" {
   security_group_id = aws_security_group.alb_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+  cidr_ipv4   = var.alb_ingress_http_cidr_ipv4
+  from_port   = var.alb_ingress_http_from_port
+  ip_protocol = var.alb_ingress_http_ip_protocol
+  to_port     = var.alb_ingress_http_to_port
 }
 
 resource "aws_vpc_security_group_ingress_rule" "alb_ingress_https" {
   security_group_id = aws_security_group.alb_sg.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 443
-  ip_protocol = "tcp"
-  to_port     = 443
+  cidr_ipv4   = var.alb_ingress_https_cidr_ipv4
+  from_port   = var.alb_ingress_https_from_port
+  ip_protocol = var.alb_ingress_https_ip_protocol
+  to_port     = var.alb_ingress_https_to_port
 }
 
 resource "aws_vpc_security_group_egress_rule" "alb_egress" {
   security_group_id = aws_security_group.alb_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+  cidr_ipv4         = var.alb_egress_cidr_ipv4
+  ip_protocol       = var.alb_egress_ip_protocol 
 }
 
---
+++ terraform/modules/ALB/variables.tf
@@ -0,0 +1,144 @@
+variable "alb-ecs-name" {
+  type        = string
+  default     = "alb-ecs"
+}
+
+variable "alb-ecs-internal" {
+  type        = bool
+  default     = false
+}
+
+variable "alb-ecs-load_balancer_type" {
+  type        = string
+  default     = "application"
+}
+
+variable "alb-ecs-enable_deletion_protection" {
+  type        = bool
+  default     = false
+}
+
+variable "aws_lb_target_group_name" {
+  type        = string
+  default     = "albtargetgroup"
+}
+
+variable "aws_lb_target_group_port" {
+  type        = number
+  default     = 80
+}
+
+variable "aws_lb_target_group_protocol" {
+  type        = string
+  default     = "HTTP"
+}
+
+variable "aws_lb_target_group_target_type" {
+  type        = string
+  default     = "ip"
+}
+
+variable "health_check_enabled" {
+  type        = bool
+  default     = true
+}
+
+variable "health_check_interval" {
+  type        = number
+  default     = 30
+}
+
+variable "health_check_path" {
+  type        = string
+  default     = "/"
+}
+
+variable "health_check_port" {
+  type        = string
+  default     = "traffic-port"
+}
+
+variable "health_check_protocol" {
+  type        = string
+  default     = "HTTP"
+}
+
+variable "health_check_timeout" {
+  type        = number
+  default     = 5
+}
+
+variable "health_check_healthy_threshold" {
+  type        = number
+  default     = 3
+}
+
+variable "health_check_unhealthy_threshold" {
+  type        = number
+  default     = 3
+}
+
+variable "health_check_matcher" {
+  type        = string
+  default     = "200-299"
+}
+
+variable "HTTPS_l_ssl_policy" {
+  type = string
+  default = "ELBSecurityPolicy-2016-08"
+}
+
+variable "aws_security_group_name" {
+  type = string
+  default = "alb_sg"
+}
+
+variable "alb_ingress_http_cidr_ipv4" {
+  type = string
+  default = "0.0.0.0/0"
+}
+
+variable "alb_ingress_http_from_port" {
+  type = number
+  default = 80
+}
+
+variable "alb_ingress_http_ip_protocol" {
+  type = string
+  default = "tcp"
+}
+
+variable "alb_ingress_http_to_port" {
+  type = number
+  default = 80
+}
+
+variable "alb_ingress_https_cidr_ipv4" {
+  type = string
+  default = "0.0.0.0/0"
+}
+
+variable "alb_ingress_https_from_port" {
+  type = number
+  default = 443
+}
+
+variable "alb_ingress_https_ip_protocol" {
+  type = string
+  default = "tcp"
+}
+
+variable "alb_ingress_https_to_port" {
+  type = number
+  default = 443
+}
+
+variable "alb_egress_cidr_ipv4" {
+  type = string
+  default = "0.0.0.0/0"
+}
+
+variable "alb_egress_ip_protocol" {
+  type = string
+  default = "-1" 
+}
