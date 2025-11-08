af66ab3903dee4d593931b3ac802bdabbeaa1cd1
Endrit-Selita <eselita2001@gmail.com>
2025-11-08 00:00:00 +0000

    added variables to VPC  module

--- terraform/modules/VPC/main.tf
+++ terraform/modules/VPC/main.tf
@@ -1,6 +1,6 @@
 ############### Create a VPC ###############
 resource "aws_vpc" "vpc-ecs" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block = var.vpc-ecs_cider_block
 }
 
 # Interet Gateway
@@ -14,20 +14,20 @@ resource "aws_internet_gateway" "i-gateway" {
 
 # availability Zone 
 data "aws_availability_zones" "az" {
-  state = "available"
+  state = var.az_state
 }
 
 # public subnet 1
 resource "aws_subnet" "public1" {
-  cidr_block              = "10.10.1.0/24"
+  cidr_block              = var.public_subnet_1_cidr_block
   vpc_id                  = aws_vpc.vpc-ecs.id
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = var.public_subnet_1_map_public_ip_on_launch
   availability_zone = data.aws_availability_zones.az.names[0]
 }
 
  # Elastic IP for Nat gateway in public subnet 1
 resource "aws_eip" "eip1" {
-  domain   = "vpc"
+  domain   = var.eip1_domain
 }
 
 # NAT gateway - public subnet 1
@@ -38,15 +38,15 @@ resource "aws_nat_gateway" "nat-gw1" {
 
 # Public subnet 2
 resource "aws_subnet" "public2" {
-  cidr_block              = "10.10.2.0/24"
+  cidr_block              = var.public_subnet_2_cidr_block
   vpc_id                  = aws_vpc.vpc-ecs.id
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = var.public_subnet_2_map_public_ip_on_launch
   availability_zone = data.aws_availability_zones.az.names[1]
 }
 
  # Elastic IP for Nat gateway in public subnet 2
 resource "aws_eip" "eip2" {
-  domain   = "vpc"
+  domain   = var.eip2_domain
 }
 
 # NAT gateway - public subnet 2
@@ -57,19 +57,21 @@ resource "aws_nat_gateway" "nat-gw2" {
 
 # private subnet 1 in AZ1
 resource "aws_subnet" "private1" {
-  cidr_block              = "10.10.3.0/24"
+  cidr_block              = var.private_subnet_1_cidr_block
   vpc_id                  = aws_vpc.vpc-ecs.id
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = var.private_subnet_1_map_public_ip_on_launch
   availability_zone = data.aws_availability_zones.az.names[0]
 }
 
 # private subnet 2 in AZ2
 resource "aws_subnet" "private2" {
-  cidr_block              = "10.10.4.0/24"
+  cidr_block              = var.private_subnet_2_cidr_block
   vpc_id                  = aws_vpc.vpc-ecs.id
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = var.private_subnet_2_map_public_ip_on_launch
   availability_zone = data.aws_availability_zones.az.names[1]
 }
+
+####### carry on from here
 
 # route table internet gateway
 resource "aws_route_table" "ig_rt_public" {
---
+++ terraform/modules/VPC/variables.tf
@@ -0,0 +1,59 @@
+variable "vpc-ecs_cider_block" {
+    type = string
+    default = "10.10.0.0/16"
+}
+
+variable "az_state" {
+    type = string
+    default = "available"
+}
+
+variable "public_subnet_1_cidr_block" {
+    type = string
+    default = "10.10.1.0/24"
+}
+
+variable "public_subnet_1_map_public_ip_on_launch" {
+    type = bool
+    default = true
+}
+
+variable "eip1_domain" {
+    type = string
+    default = "vpc"
+}
+
+variable "public_subnet_2_cidr_block" {
+    type = string
+    default = "10.10.2.0/24"
+}
+
+variable "public_subnet_2_map_public_ip_on_launch" {
+    type = bool
+    default = true
+}
+
+variable "eip2_domain" {
+    type = string
+    default = "vpc"
+}
+
+variable "private_subnet_1_cidr_block" {
+    type = string
+    default = "10.10.3.0/24"
+}
+
+variable "private_subnet_1_map_public_ip_on_launch" {
+    type = bool
+    default = false
+}
+
+variable "private_subnet_2_cidr_block" {
+    type = string
+    default = "10.10.4.0/24"
+}
+
+variable "private_subnet_2_map_public_ip_on_launch" {
+    type = bool
+    default = false
+}
