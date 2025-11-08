2c0a4be002b0bb49e1a7108f7ce2d13f3cc974cc
Endrit-Selita <eselita2001@gmail.com>
2025-11-08 10:10:06 +0000

    added variables and ouputs to route 53  module

---
+++ terraform/modules/Route 53/outputs.tf
@@ -0,0 +1,11 @@
+output "aws_route53_zone_name" {
+  value = aws_route53_zone.r53_zone.name
+}
+
+output "aws_route53_record_name" {
+  value = aws_route53_record.r53_record.name
+}
+
+output "aws_route53_fqdn" {
+  value = aws_route53_record.r53_record.fqdn
+}
